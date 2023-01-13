d9d9ee51a13ef8d9351bb8faf81ea307cb8e317a
Diego Lema <Diegoariel3@gmail.com>
2023-01-13 18:46:41 -0300

    Finalizacin del CV y agregado de JS

---
+++ main.js
@@ -0,0 +1,32 @@
+document.getElementById("contact").onclick = function() {
+    var ocultar = document.getElementById("contact_information");
+    if (ocultar.style.display === "none") {
+      ocultar.style.display = "block";
+    } else {
+      ocultar.style.display = "none";
+    }
+  }
+  document.getElementById("experience").onclick = function() {
+    var ocultar = document.getElementById("experience_box");
+    if (ocultar.style.display === "none") {
+      ocultar.style.display = "block";
+    } else {
+      ocultar.style.display = "none";
+    }
+  }
+  document.getElementById("studies").onclick = function() {
+    var ocultar = document.getElementById("studies_box");
+    if (ocultar.style.display === "none") {
+      ocultar.style.display = "block";
+    } else {
+      ocultar.style.display = "none";
+    }
+  }
+  document.getElementById("target").onclick = function() {
+    var ocultar = document.getElementById("target_box");
+    if (ocultar.style.display === "none") {
+      ocultar.style.display = "block";
+    } else {
+      ocultar.style.display = "none";
+    }
+  }
